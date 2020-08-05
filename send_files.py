8b3235f12ede8901d1f98b43a0c0b1a79f728f52
zarif101 <zarif.azher@gmail.com>
2020-08-05 14:36:37 -0700

    Working on deployment

--- send_files.py
+++ send_files.py
@@ -20,38 +20,40 @@ class Email():
         self.sender_password = sender_password
 
     def send_email(self):
+        try:
+            message = MIMEMultipart()
+            message['From'] = self.sender_email
+            message['To'] = self.receiver_email
+            message['Subject'] = self.subject
 
-        message = MIMEMultipart()
-        message['From'] = self.sender_email
-        message['To'] = self.receiver_email
-        message['Subject'] = self.subject
+            message.attach(MIMEText(self.message,'plain'))
+            st.write('Getting ready to send!')
+            with open(self.model_path+self.model_name,'rb') as file:
+                try:
+                    part = MIMEApplication(file.read(),name=self.model_name)
+                    part['Content-Disposition'] = 'attachment; filename="%s"' % self.model_name
+                    message.attach(part)
+                except Exception as e:
+                    st.write(e)
+                    #print(type(message))
+                    #print(message)
+                    #print(e)
 
-        message.attach(MIMEText(self.message,'plain'))
-        st.write('Getting ready to send!')
-        with open(self.model_path+self.model_name,'rb') as file:
-            try:
-                part = MIMEApplication(file.read(),name=self.model_name)
-                part['Content-Disposition'] = 'attachment; filename="%s"' % self.model_name
-                message.attach(part)
-            except Exception as e:
-                st.write(e)
-                #print(type(message))
-                #print(message)
-                #print(e)
-
-        context = ssl.create_default_context()
-        text = message.as_string()
-        st.write('Almost sent!')
-        #print('ready to send!')
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-            #print('here we go!')
-            #print(self.sender_email, self.sender_password)
-            server.login(self.sender_email, self.sender_password)
-            #print('logged in!')
-            try:
-                server.sendmail(self.sender_email, self.receiver_email, text)
-                #print('sent!')
-            except Exception as e:
-                st.write(e)
-                #print('couldnt send')
-                #print(e)
+            context = ssl.create_default_context()
+            text = message.as_string()
+            st.write('Almost sent!')
+            #print('ready to send!')
+            with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+                #print('here we go!')
+                #print(self.sender_email, self.sender_password)
+                server.login(self.sender_email, self.sender_password)
+                #print('logged in!')
+                try:
+                    server.sendmail(self.sender_email, self.receiver_email, text)
+                    #print('sent!')
+                except Exception as e:
+                    st.write(e)
+                    #print('couldnt send')
+                    #print(e)
+        except Exception as f:
+            st.write(f)
