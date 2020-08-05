7d88659654ecf8943e789814621fdb36d8f3cfd6
zarif101 <zarif.azher@gmail.com>
2020-08-05 12:04:43 -0700

    Working on deployment

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import seaborn as sns
-import cv2
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression,LogisticRegression
 from sklearn.metrics import classification_report,confusion_matrix
